bf983f4e095c191c70d39aaafa681fa466417732
MGBradley <bradleymary@gmail.com>
2023-06-30 15:24:38 -0400

    Update catsection.js

--- catsection.js
+++ catsection.js
@@ -19,17 +19,4 @@ grist.onRecord(function (record) {
     document.getElementById('image').src = record.Link;
   });
 
-const data = {
-  count: 0,
-  section: '',
-  status: 'waiting',
-  tableConnected: false,
-  rowConnected: false,
-  haveRows: false,
-};
-let app = undefined;
 
-app = new Vue({
-    el: '#app',
-    data: data
-  });
